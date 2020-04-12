6673b00c9935722194c6f1b2fbc3dc1c1e362992
markus <markus@markusessig.de>
2020-04-13 01:51:20 +0200

    Fix logging

--- convert_opencv.sh
+++ convert_opencv.sh
@@ -3,10 +3,10 @@
 
 convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
 
-echo "run image processing on ${11} using opencv"
+echo "run image processing on ${11} using opencv" >> /image_processing.log
 
 /app/image_processing.py -i ${11}
 
 cp ${11} /app/$(basename -- ${11})
 
-echo "finish image processing on ${11} using opencv"
+echo "finish image processing on ${11} using opencv" >> /image_processing.log
