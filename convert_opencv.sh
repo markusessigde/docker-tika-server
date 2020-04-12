bf65eda944cfdd766c9f2dfcb54c91a78bfe1203
markus <markus@markusessig.de>
2020-04-12 20:34:27 +0200

    Fix call of python scrip for image processing with opencv

--- convert_opencv.sh
+++ convert_opencv.sh
@@ -5,6 +5,6 @@ convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
 
 echo "run image processing on ${11} using opencv"
 
-./image_processing.py ${11}
+/app/image_processing.py ${11}
 
 echo "finish image processing on ${11} using opencv"
