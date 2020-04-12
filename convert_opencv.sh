bac47a4afe294a018d61417b623bcf10deada5db
markus <markus@markusessig.de>
2020-04-13 01:59:47 +0200

    Add more convert parameters

--- convert_opencv.sh
+++ convert_opencv.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-echo "called convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}" >> /image_processing.log
+echo "called convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14}" >> /image_processing.log
 
-convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
+convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} 
 
 echo "run image processing on ${11} using opencv" >> /image_processing.log
 
-/app/image_processing.py -i ${11}
+/app/image_processing.py -i ${13}
 
-cp ${11} /app/$(basename -- ${11})
+cp ${13} /app/$(basename -- ${13})
 
-echo "finish image processing on ${11} using opencv" >> /image_processing.log
+echo "finish image processing on ${13} using opencv" >> /image_processing.log
