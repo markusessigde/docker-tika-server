b8c99598300e7244f0eb0a1bac4c8d18d09cff45
markus <markus@markusessig.de>
2020-04-12 19:39:37 +0200

    Add image with image preprocessing using opencv

--- convert_opencv.sh
+++ convert_opencv.sh
@@ -3,4 +3,8 @@
 
 convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
 
+echo "run image processing on ${11} using opencv"
+
 ./image_processing.py ${11}
+
+echo "finish image processing on ${11} using opencv"
