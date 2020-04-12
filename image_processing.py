f858e801dbb81ff26ffd52640d98057068e1b6dd
markus <markus@markusessig.de>
2020-04-12 14:30:46 +0200

    Add image with image preprocessing using opencv

---
+++ image_processing.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import cv2
+import tempfile
+from PIL import Image
+import imutils
+import numpy as np
+import argparse
+
+IMAGE_SIZE = 1800
+BINARY_THREHOLD = 180
+
+
+def set_image_dpi(file_path):
+    im = Image.open(file_path)
+    length_x, width_y = im.size
+    factor = min(1, float(1024.0 / length_x))
+    size = int(factor * length_x), int(factor * width_y)
+    im_resized = im.resize(size, Image.ANTIALIAS)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
+    temp_filename = temp_file.name
+    im_resized.save(temp_filename, dpi=(300, 300))
+    return temp_filename
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image file")
+args = vars(ap.parse_args())
+
+img = process_image_for_ocr(args["image"])
+
+cv2.imwrite(args["image"], img)
+
+def process_image_for_ocr(file_path):
+    print('Processing image for text Extraction')
+    temp_filename = set_image_dpi(file_path)
+    im_new = remove_noise_and_smooth(temp_filename)
+    return im_new
+
+
+def image_smoothening(img):
+    ret1, th1 = cv2.threshold(img, BINARY_THREHOLD, 255, cv2.THRESH_BINARY)
+    ret2, th2 = cv2.threshold(th1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(th2, (1, 1), 0)
+    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return th3
+
+
+def remove_noise_and_smooth(file_name):
+    print('Removing noise and smoothening image')
+    img = cv2.imread(file_name, 0)
+    filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 41, 3)
+    kernel = np.ones((1, 1), np.uint8)
+    opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    img = image_smoothening(img)
+    or_image = cv2.bitwise_or(img, closing)
+    return or_image
