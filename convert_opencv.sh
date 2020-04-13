0fa989a02ef8b50f8451f17069aaaae88100fa9e
markus <markus@markusessig.de>
2020-04-13 16:25:45 +0200

    Refine logging

--- convert_opencv.sh
+++ convert_opencv.sh
@@ -1,13 +1,3 @@
 #! /bin/bash
 
-echo "called convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14}" >> /image_processing.log
-
-#convert $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} 
-
-echo "run image processing on ${11} using opencv" >> /image_processing.log
-
 /app/image_processing.py -i ${13}
-
-cp ${13} /app/$(basename -- ${13})
-
-echo "finish image processing on ${13} using opencv" >> /image_processing.log
--- image_processing.py
+++ image_processing.py
@@ -16,17 +16,12 @@ BINARY_THREHOLD = 180
 logger = logging.getLogger('image_processing')
 logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('image_processing.log')
-fh.setLevel(logging.DEBUG)
-
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
-logger.addHandler(fh)
 logger.addHandler(ch)
 
 
